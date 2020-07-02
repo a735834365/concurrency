b8f501cc84743548aecb4553abe3bd27a8d497c6
yfengJ <735834365@qq.com>
2020-07-02 17:10:20 +0800

    在没有同步的情况下共享变量（不要这么做）

--- src/main/java/com/zyf/concurrency/chapter03/NoVisibility.java
+++ src/main/java/com/zyf/concurrency/chapter03/NoVisibility.java
@@ -1,13 +1,17 @@
 package com.zyf.concurrency.chapter03;
 
 /**
- * 因为在代码中没有使用足够的同步机制，因此无站保证主线程写入的
+ * 在没有同步的情况下共享变量（不要这么做）
+ * 引自原文：
+ *      因为在代码中没有使用足够的同步机制，因此无站保证主线程写入的
  * ready值和number相对读线程来说是可见的。
  *
  * create by yifeng
  */
 public class NoVisibility {
 
+    // volatile将保证可见性
+//    private static volatile boolean ready;
     private static boolean ready;
     private static int number;
 
@@ -23,8 +27,10 @@ public class NoVisibility {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new ReaderThread().start();
+        // 模拟，如果主线程执行慢了，则ReaderThread将不会停止
+        Thread.currentThread().wait(1000);
         number = 42;
         ready = true;
     }
